ded14caab5518fb43079fa9d0e3ee8d68523c46d
John Todd <jtodd2854@gmail.com>
2019-12-07 16:34:13 -0500

    solve the second part

--- 2019/04/cracker.py
+++ 2019/04/cracker.py
@@ -2,16 +2,22 @@ import sys
 
 def hack(guess):
     str_guess = str(guess)
-    repeats = 0
+    repeats = {}
     prev_digit = int(str_guess[0])
     for i in range(1, len(str_guess)):
         this_digit = int(str_guess[i])
         if this_digit < prev_digit:
             return False
         if this_digit == prev_digit:
-            repeats = repeats + 1
+            if this_digit not in repeats:
+                repeats[this_digit] = 2
+            else:
+                repeats[this_digit] = repeats[this_digit] + 1
         prev_digit = this_digit
-    return repeats > 0
+    for repeat in repeats.values():
+        if repeat == 2:
+            return True
+    return False
 
 def main():
     min, max, *_ = sys.argv[1:]
--- 2019/04/test_cracker.py
+++ 2019/04/test_cracker.py
@@ -1,6 +1,12 @@
 import cracker
 
 def test_cracker():
-    assert     cracker.hack('111111')
+    assert not cracker.hack('111111')
     assert not cracker.hack('223450')
     assert not cracker.hack('123789')
+
+def test_cracker_extended():
+    assert     cracker.hack('112233')
+    assert not cracker.hack('123444')
+    assert     cracker.hack('111122')
+    assert not cracker.hack('122223')
