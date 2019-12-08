2326799a42c28abdb91fc4bc8211396e336dc488
John Todd <jtodd2854@gmail.com>
2019-12-07 19:04:59 -0500

    refactor

--- 2019/Intcode/intcode.py
+++ 2019/Intcode/intcode.py
@@ -1,42 +1,105 @@
 import sys
 
-def read_memory(filename):
-    fp = open(filename, 'r')
-    line = fp.readline().strip()
-    fp.close()
-    return line.split(',')
+from abc import ABC, abstractmethod
 
-def run_program(memory):
-    instr_ptr = 0
-    while memory[instr_ptr] != '99':
-        instruction = memory[instr_ptr : instr_ptr + 4]
-        instr_ptr = process_instruction(instr_ptr, instruction, memory)
+class Instruction(ABC):
+    def __init__(self, parameters=None):
+        self.parameters = parameters if parameters else []
 
-def process_instruction(instr_ptr, instruction, memory):
-    opcode = int(instruction[0])
-    parm_1 = int(instruction[1])
-    parm_2 = int(instruction[2])
-    parm_3 = int(instruction[3])
-    arg1 = int(memory[parm_1])
-    arg2 = int(memory[parm_2])
-    if opcode == 1:
-        memory[parm_3] = str(arg1 + arg2)
-    elif opcode == 2:
-        memory[parm_3] = str(arg1 * arg2)
-    return instr_ptr + 4
+    @abstractmethod
+    def name(self):
+        pass
+
+    @abstractmethod
+    def length():
+        pass
     
-def write_memory(memory):
-    print(','.join(memory))
+    @abstractmethod
+    def act(self, memory):
+        pass
+
+    @staticmethod
+    def create_instruction(instr_ptr, memory):
+        if memory[instr_ptr] == '99':
+            new_instr_ptr = instr_ptr + Stop.length()
+            return (new_instr_ptr, Stop())
+        elif memory[instr_ptr] == '1':
+            parameter_start = instr_ptr + 1
+            new_instr_ptr = instr_ptr + Addition.length()
+            return (new_instr_ptr, Addition(memory[parameter_start : new_instr_ptr]))
+        elif memory[instr_ptr] == '2':
+            parameter_start = instr_ptr + 1
+            new_instr_ptr = instr_ptr + Multiplication.length()
+            return (new_instr_ptr, Multiplication(memory[parameter_start : new_instr_ptr]))
+
+class Stop(Instruction):
+    def name(self):
+        return 'Stop'
+
+    @staticmethod
+    def length():
+        return 1
+
+    def act(self, memory):
+        pass
+
+class Addition(Instruction):
+    def name(self):
+        return 'Addition'
+
+    @staticmethod
+    def length():
+        return 4
+
+    def act(self, memory):
+        arg1 = int(memory[int(self.parameters[0])])
+        arg2 = int(memory[int(self.parameters[1])])
+        memory[int(self.parameters[2])] = str(arg1 + arg2)
+
+class Multiplication(Instruction):
+    def name(self):
+        return 'Multiplication'
+
+    @staticmethod
+    def length():
+        return 4
+
+    def act(self, memory):
+        arg1 = int(memory[int(self.parameters[0])])
+        arg2 = int(memory[int(self.parameters[1])])
+        memory[int(self.parameters[2])] = str(arg1 * arg2)
+
+class Intcode():
+
+    def __init__(self):
+        self.memory = []
+
+    def read_memory(self, filename):
+        fp = open(filename, 'r')
+        line = fp.readline().strip()
+        fp.close()
+        self.memory = line.split(',')
+
+    def run_program(self):
+        self.instr_ptr = 0
+        current_instr = None
+        while current_instr == None or current_instr.name() is not 'Stop':
+            self.instr_ptr, current_instr = Instruction.create_instruction(self.instr_ptr, self.memory)
+            current_instr.act(self.memory)
+
+    def write_memory(self):
+        print(','.join(self.memory))
 
 def main():
+    intcode = Intcode()
     input_file = sys.argv[1]
     print('Booting up Intcode')
     print(f"Reading {input_file} into memory")
-    memory = read_memory(input_file)
+    intcode.read_memory(input_file)
     print(f"Running {input_file}")
-    run_program(memory)
+    intcode.run_program()
     print(f"Writing results to stdout")
-    write_memory(memory)
+    intcode.write_memory()
 
 if __name__ == '__main__':
     main()
