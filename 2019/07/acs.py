d64ec88b701cf91a5f571f1e4f9112bbaf094878
John Todd <j.todd@accenture.com>
2019-12-12 16:29:56 -0500

    series of intcodes, reproduces the described results on test data

---
+++ 2019/07/acs.py
@@ -0,0 +1,27 @@
+import sys
+import intcode.intcode
+
+def main():
+    settings = [0, 1, 2, 3, 4]
+    program = sys.argv[1]
+    input_buffers = [
+        [str(settings[1])],
+        [str(settings[0])],
+        [str(settings[4])],
+        [str(settings[3])],
+        [str(settings[2])]
+    ]
+    output_buffers = [[], [], [], [], []]
+    ss = []
+    value = '0'
+    for i in range(5):
+        ss.append(intcode.intcode.Intcode(input_device=input_buffers[i], output_device=output_buffers[i]))
+        ss[i].read_memory(program)
+        input_buffers[i].append(value)
+        ss[i].run_program()
+        value = str(output_buffers[i][0])
+    print(output_buffers[4][0])
+
+
+if __name__ == '__main__':
+    main()
