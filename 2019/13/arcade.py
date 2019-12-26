ac28c14251d6d8d3cb766f59004622e7f44d2593
John Todd <j.todd@accenture.com>
2019-12-26 14:00:10 -0500

    part 2 of day 13

--- 2019/13/arcade.py
+++ 2019/13/arcade.py
@@ -1,5 +1,7 @@
 import sys
+import time
 from queue import Queue
+from threading import Thread
 from intcode.intcode import Intcode
 
 
@@ -8,18 +10,49 @@ WALL = 1
 BLOCK = 2
 PADDLE = 3
 BALL = 4
+SCORE = 5
+LEFT = -1
+NEUTRAL = 0
+RIGHT = 1
+
+
+def read_display(buffer):
+    my_display = {}
+    while not buffer.empty():
+        x = buffer.get()
+        y = buffer.get()
+        sprite = buffer.get()
+        if x == -1 and y == 0:
+            my_display[SCORE] = sprite
+        else:
+            my_display[sprite] = my_display.get(sprite, []) + [(x, y)]
+    return my_display
 
 
 if __name__ == '__main__':
     program = Intcode.read_program(sys.argv[1])
+    program[0] = 2
+    input_buffer = Queue()
+    input_buffer.put(NEUTRAL)
     output_buffer = Queue()
-    ss = Intcode(output_device=output_buffer)
+    ss = Intcode(input_device=input_buffer, output_device=output_buffer)
     ss.load_program(program)
-    ss.run_program()
+    t = Thread(target=ss.run_program)
+    t.start()
     display = {}
-    while not output_buffer.empty():
-        x = output_buffer.get()
-        y = output_buffer.get()
-        sprite = output_buffer.get()
-        display[sprite] = display.get(sprite, 0) + 1
-    print(f"Number of blocks: {display[BLOCK]}\n")
+    while t.is_alive():
+        time.sleep(0.1)
+        display = read_display(output_buffer)
+        if BALL in display and PADDLE in display:
+            ball_position = display[BALL][0][0]
+            paddle_position = display[PADDLE][0][0]
+            if ball_position < paddle_position:
+                input_buffer.put(LEFT)
+            elif ball_position > paddle_position:
+                input_buffer.put(RIGHT)
+            else:
+                input_buffer.put(NEUTRAL)
+        else:
+            input_buffer.put(NEUTRAL)
+    t.join()
+    print(f"Final score: {display[SCORE]}")
