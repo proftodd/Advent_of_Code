17b53a828f138f724dd85c8f500a283141ad62e4
John Todd <jtodd2854@gmail.com>
2020-01-02 18:09:33 -0500

    refactor

--- 2019/15/maze_runner.py
+++ 2019/15/maze_runner.py
@@ -19,16 +19,12 @@ MAX_DEPTH = None
 
 
 def copy_path(path, next_step=None):
+    last_step = path.queue[-1] if len(list(path.queue)) > 0 else None
     path_copy = Queue()
-    last_step = None
-    queue_size = 0
-    for s in path.queue:
-        queue_size = queue_size + 1
-        last_step = s
-        path_copy.put(last_step)
+    path_copy.queue.extend(list(path.queue))
     if next_step is not None:
         path_copy.put(next_step)
-    return queue_size, last_step, path_copy
+    return last_step, path_copy
 
 
 def try_direction(my_program, path_to_try):
@@ -51,16 +47,16 @@ def opposite_of(step):
 
 
 def try_a_step(the_program, path_so_far):
-    path_length, last_step, path_copy = copy_path(path_so_far, HALT)
+    last_step, path_copy = copy_path(path_so_far, HALT)
     this_result = try_direction(list(the_program), path_copy)
     if this_result == TANK:
-        return [(path_length, path_so_far)]
+        return [path_so_far]
     elif this_result == WALL:
         return []
-    elif MAX_DEPTH is not None and path_length >= MAX_DEPTH:
-        return [(path_length, path_so_far)]
+    elif MAX_DEPTH is not None and len(list(path_so_far.queue)) >= MAX_DEPTH:
+        return [path_so_far]
     candidate_directions = [d for d in directions if d != opposite_of(last_step)]
-    candidate_paths = [copy_path(path_so_far, d)[2] for d in candidate_directions]
+    candidate_paths = [copy_path(path_so_far, d)[1] for d in candidate_directions]
     next_steps = [try_a_step(the_program, p) for p in candidate_paths]
     possible_solutions = [p for pl in next_steps for p in pl]
     return possible_solutions
@@ -68,9 +64,9 @@ def try_a_step(the_program, path_so_far):
 
 if __name__ == '__main__':
     program = Intcode.read_program(sys.argv[1])
-    starting_paths = [copy_path(Queue(), d)[2] for d in directions]
+    starting_paths = [copy_path(Queue(), d)[1] for d in directions]
     solutions = [try_a_step(list(program), p) for p in starting_paths]
     true_solutions = [p for pl in solutions for p in pl]
     for the_solution in true_solutions:
-        print(f"Minimum number of steps: {the_solution[0]}")
-        print(f"The solution is : {','.join(map(str, the_solution[1].queue))}")
+        print(f"Minimum number of steps: {len(list(the_solution.queue))}")
+        print(f"The solution is : {','.join(map(str, the_solution.queue))}")
