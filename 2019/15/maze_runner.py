9e0f2b45a96bfdca75757e6db5039f7f0f5aab74
John Todd <j.todd@accenture.com>
2020-01-02 15:52:19 -0500

    solve the branching problem by implementing flatmap, solve part 1

--- 2019/15/maze_runner.py
+++ 2019/15/maze_runner.py
@@ -15,7 +15,7 @@ CLEAR = 1
 TANK = 2
 
 
-MAX_DEPTH = 79
+MAX_DEPTH = None
 
 
 def copy_path(path, next_step=None):
@@ -54,27 +54,23 @@ def try_a_step(the_program, path_so_far):
     path_length, last_step, path_copy = copy_path(path_so_far, HALT)
     this_result = try_direction(list(the_program), path_copy)
     if this_result == TANK:
-        return path_length, path_so_far
+        return [(path_length, path_so_far)]
     elif this_result == WALL:
-        return 0, None
-    elif path_length >= MAX_DEPTH:
-        return path_length, path_so_far
+        return []
+    elif MAX_DEPTH is not None and path_length >= MAX_DEPTH:
+        return [(path_length, path_so_far)]
     candidate_directions = [d for d in directions if d != opposite_of(last_step)]
     candidate_paths = [copy_path(path_so_far, d)[2] for d in candidate_directions]
     next_steps = [try_a_step(the_program, p) for p in candidate_paths]
-    possible_solutions = list(filter(lambda p: p[0] > 0, next_steps))
-    if len(possible_solutions) < 1:
-        print(f"uh, oh, no solution found after {path_length} steps")
-        print(f"The path so far is {','.join(map(str, path_so_far.queue))}")
-        print(f"The last step I tried is {last_step}")
-        raise Exception()
-    return possible_solutions[0]
+    possible_solutions = [p for pl in next_steps for p in pl]
+    return possible_solutions
 
 
 if __name__ == '__main__':
     program = Intcode.read_program(sys.argv[1])
     starting_paths = [copy_path(Queue(), d)[2] for d in directions]
     solutions = [try_a_step(list(program), p) for p in starting_paths]
-    the_solution = list(filter(lambda p: p[0] > 0, solutions))[0]
-    print(f"Minimum number of steps: {the_solution[0]}")
-    print(f"The solution is : {','.join(map(str, the_solution[1].queue))}")
+    true_solutions = [p for pl in solutions for p in pl]
+    for the_solution in true_solutions:
+        print(f"Minimum number of steps: {the_solution[0]}")
+        print(f"The solution is : {','.join(map(str, the_solution[1].queue))}")
