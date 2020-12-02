c6f60329ec8a0d7b52c0d77bf839bb76abdcd674
John Todd <jtodd2854@gmail.com>
2020-12-01 19:39:38 -0500

    solution to day 1 part 2

--- AOC2020/Day_01/Program.cs
+++ AOC2020/Day_01/Program.cs
@@ -12,7 +12,9 @@ namespace Day_01
                 .Select(s => Int32.Parse(s))
                 .ToArray();
             var (e1, e2, product) = CorrectExpenseReport(entries);
+            var (b1, b2, b3, bonus) = FindBonus(entries);
             Console.WriteLine($"{e1} * {e2} = {product}");
+            Console.WriteLine($"{b1} * {b2} * {b3} = {bonus}");
         }
 
         public static ValueTuple<int, int, int> CorrectExpenseReport(int[] entries)
@@ -30,6 +32,26 @@ namespace Day_01
             }
 
             return (0, 0, 0);
+        }
+
+        public static ValueTuple<int, int, int, int> FindBonus(int[] entries)
+        {
+            int i, j, k;
+            for (i = 0; i < entries.Length - 2; ++i)
+            {
+                for (j = i + 1; j < entries.Length - 1; ++j)
+                {
+                    for (k = j + 1; k < entries.Length; ++k)
+                    {
+                        if (entries[i] + entries[j] + entries[k] == 2020)
+                        {
+                            return (entries[i], entries[j], entries[k], entries[i] * entries[j] * entries[k]);
+                        }
+                    }
+                }
+            }
+
+            return (0, 0, 0, 0);
         }
     }
 }
--- AOC2020/Test_Day_01/TestExpenseReport.cs
+++ AOC2020/Test_Day_01/TestExpenseReport.cs
@@ -4,18 +4,25 @@ namespace Day_01
 {
     public class Test_Day_01_Part_1
     {
+        int[] input = {
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456
+        };
+
         [Test]
         public void It_corrects_expense_report()
         {
-            int[] input = {
-                1721,
-                979,
-                366,
-                299,
-                675,
-                1456
-            };
             Assert.AreEqual((1721, 299, 514579), Program.CorrectExpenseReport(input));
+        }
+
+        [Test]
+        public void It_finds_bonus_coin()
+        {
+            Assert.AreEqual((979, 366, 675, 241861950), Program.FindBonus(input));
         }
     }
 }
