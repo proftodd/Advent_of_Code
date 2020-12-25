241dfef4e707ec52685d6244c4aece5da0ca4c5e
John Todd <jtodd2854@gmail.com>
2020-12-25 16:05:42 -0500

    solution to day 2 part 2

--- AOC2020/Day_09/Program.cs
+++ AOC2020/Day_09/Program.cs
@@ -10,14 +10,16 @@ namespace Day_09
         {
             var preambleLength = int.Parse(args[0]);
             var data = File.ReadLines(args[1])
-                .Select(Int64.Parse)
+                .Select(long.Parse)
                 .ToArray();
             var preamble = new long[preambleLength];
             Array.Copy(data, 0, preamble, 0, preambleLength);
 
             Console.WriteLine($"The preamble has length {preambleLength}");
-            Console.WriteLine($"the first preamble is {string.Join(',', preamble)}");
+            // Console.WriteLine($"the first preamble is {string.Join(',', preamble)}");
 
+            long key = 0L;
+            long weakness = 0L;
             for (int i = preambleLength; i < data.Length;)
             {
                 var cp =
@@ -33,10 +35,30 @@ namespace Day_09
                 }
                 else
                 {
-                    Console.WriteLine($"{data[i]} cannot be formed as the sum of any two elements of {string.Join(',', preamble)}");
+                    key = data[i];
                     break;
                 }
             }
+            Console.WriteLine($"{key} cannot be formed as the sum of any two elements of {string.Join(',', preamble)}");
+
+            for (int i = 2; i <= data.Length; ++i)
+            {
+                for (int j = 0; j <= data.Length - i; ++j)
+                {
+                    var thisRange = data[j .. (i + j)];
+                    // Console.WriteLine(string.Join(',', thisRange));
+                    if (thisRange.Sum() == key)
+                    {
+                        // Console.WriteLine($"{key} = {string.Join('+', thisRange)}");
+                        Array.Sort(thisRange);
+                        weakness = thisRange[0] + thisRange[^1];
+                        goto FOUND;
+                    }
+                }
+            }
+            FOUND:
+            Console.WriteLine($"The encryption weakness is {weakness}");
+
         }
     }
 }
