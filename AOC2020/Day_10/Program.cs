88c40a3a40784c9b6b9035ddf1ebceb3a3fb1fda
John Todd <jtodd2854@gmail.com>
2020-12-26 19:04:34 -0500

    refactor

--- AOC2020/Day_10/Program.cs
+++ AOC2020/Day_10/Program.cs
@@ -14,33 +14,30 @@ namespace Day_10
                 .ToArray();
             Array.Sort(adapters);
             var differences = new Dictionary<int, int>();
-            int currentValue;
-            if (differences.TryGetValue(adapters[0], out currentValue))
-            {
-                differences[adapters[0]] = currentValue + 1;
-            }
-            else
-            {
-                differences.Add(adapters[0], 1);
-            }
+            HandleJoltageDifference(adapters[0], differences);
             for (int i = 0; i < adapters.Length - 1; ++i)
             {
                 var difference = adapters[i + 1] - adapters[i];
-                if (difference < 1 || difference > 3)
-                {
-                    throw new Exception($"cannot bridge a difference of {difference} between {adapters[i]} and {adapters[i + 1]}");
-                }
-                if (differences.TryGetValue(difference, out currentValue))
-                {
-                    differences[difference] = currentValue + 1;
-                }
-                else
-                {
-                    differences.Add(difference, 1);
-                }
+                HandleJoltageDifference(difference, differences);
             }
-            differences[3] = differences[3] + 1;
+            HandleJoltageDifference(3, differences);
             Console.WriteLine($"{differences[1]} * {differences[3]} = {differences[1] * differences[3]}");
+        }
+
+        public static void HandleJoltageDifference(int difference, IDictionary<int, int> countedDifferences)
+        {
+            if (difference < 1 || difference > 3)
+            {
+                throw new Exception($"cannot bridge a {difference} Joltage difference");
+            }
+            if (countedDifferences.TryGetValue(difference, out var currentValue))
+            {
+                countedDifferences[difference] = currentValue + 1;
+            }
+            else
+            {
+                countedDifferences.Add(difference, 1);
+            }
         }
     }
 }
