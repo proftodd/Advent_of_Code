36e58cbc34cbd2f79e1fe6cf861d1417214fcbc9
John Todd <jtodd2854@gmail.com>
2021-06-02 16:06:56 -0400

    solution to day 17 part 1

---
+++ AOC2020/prod/Day_17/ConwayField.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_17
+{
+    public class ConwayField
+    {
+        private IDictionary<(int, int, int), char> _population;
+        private int AbsMax;
+
+        private ConwayField()
+        {
+            _population = new Dictionary<(int, int, int), char>();
+        }
+
+        public ConwayField(string[] lines) : this()
+        {
+            var zLayers = PartitionLines(lines);
+            int numRows, numCols, maxX, maxY, xCoord, yCoord;
+
+            foreach (var zCoord in zLayers.Keys)
+            {
+                var theseLines = zLayers[zCoord];
+                numRows = theseLines.Length;
+                numCols = theseLines[0].Length;
+                maxX = numRows / 2;
+                maxY = numCols / 2;
+                for (int i = 0; i < numRows; ++i)
+                {
+                    var thisLine = theseLines[i];
+                    xCoord = maxX - numRows + i + 1;
+                    for (int j = 0; j < numCols; ++j)
+                    {
+                        yCoord = maxY - numRows + j + 1;
+                        if (thisLine[j] == '#')
+                        {
+                            _population.Add((xCoord, yCoord, zCoord), thisLine[j]);
+                        }
+                    }
+                }
+            }
+            AbsMax = _population.Keys.SelectMany(t => new[] { t.Item1, t.Item2, t.Item3 })
+                .Select(coord => Math.Abs(coord))
+                .Max();
+        }
+
+        public char this [(int, int, int) coord]
+        {
+            get { return _population.TryGetValue(coord, out var value) ? value : '.'; }
+            private set
+            {
+                if (value == '#')
+                {
+                    if (Math.Abs(coord.Item1) > AbsMax)
+                    {
+                        AbsMax = Math.Abs(coord.Item1);
+                    }
+                    if (Math.Abs(coord.Item2) > AbsMax)
+                    {
+                        AbsMax = Math.Abs(coord.Item2);
+                    }
+                    if (Math.Abs(coord.Item3) > AbsMax)
+                    {
+                        AbsMax = Math.Abs(coord.Item3);
+                    }
+                    _population[coord] = value;
+                }
+            }
+        }
+
+        public int MinX => -AbsMax;
+
+        public int MaxX => AbsMax;
+
+        public int MinY => -AbsMax;
+
+        public int MaxY => AbsMax;
+
+        public int MinZ => -AbsMax;
+
+        public int MaxZ => AbsMax;
+
+        public int Population => _population.Values.Count();
+
+        public int NeighborCount(int xCoord, int yCoord, int zCoord)
+        {
+            int neighborCount = 0;
+            for (int z = zCoord - 1; z <= zCoord + 1; ++z)
+            {
+                for (int y = yCoord - 1; y <= yCoord + 1; ++y)
+                {
+                    for (int x = xCoord - 1; x <= xCoord + 1; ++x)
+                    {
+                        if (x == xCoord && y == yCoord && z == zCoord)
+                        {
+                            continue;
+                        }
+                        if (this[(x, y, z)] == '#')
+                        {
+                            ++neighborCount;
+                        }
+                    }
+                }
+            }
+            return neighborCount;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            var other = (ConwayField)obj;
+            var thisActive = this._population.Where(kvp => kvp.Value == '#').ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            var otherActive = other._population.Where(kvp => kvp.Value == '#').ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            if (thisActive.Keys.Except(otherActive.Keys).Any())
+            {
+                return false;
+            }
+
+            if (otherActive.Keys.Except(thisActive.Keys).Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public ConwayField Iterate()
+        {
+            var ret = new ConwayField();
+            for (int z = MinZ - 1; z <= MaxZ + 1; ++z)
+            {
+                for (int y = MinY - 1; y <= MaxY + 1; ++y)
+                {
+                    for (int x = MinX - 1; x <= MaxX + 1; ++x)
+                    {
+                        int neighborCount = NeighborCount(x, y, z);
+                        if (this[(x, y, z)] == '#')
+                        {
+                            if (neighborCount == 2 || neighborCount == 3)
+                            {
+                                ret[(x, y, z)] = '#';
+                            }
+                        }
+                        else
+                        {
+                            if (neighborCount == 3)
+                            {
+                                ret[(x, y, z)] = '#';
+                            }
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private static IDictionary<int, string[]> PartitionLines(string[] lines)
+        {
+            var ret = new Dictionary<int, string[]>();
+            List<string> group = new List<string>();
+            var headerRegex = new Regex("^z=(-?\\d+)$");
+            Match match;
+            int zCoord = -1;
+
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                match = headerRegex.Match(lines[i]);
+                if (match.Success)
+                {
+                    zCoord = int.Parse(match.Groups[1].Value);
+                }
+                else if (lines[i] == "")
+                {
+                    ret.Add(zCoord, group.ToArray());
+                    group = new List<string>();
+                } else
+                {
+                    group.Add(lines[i]);
+                }
+            }
+            ret.Add(zCoord, group.ToArray());
+
+            return ret;
+        }
+    }
+}
---
+++ AOC2020/test/Test_Day_17/TestConwayField.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Day_17
+{
+    public class TestConwayField
+    {
+        [Test]
+        public void It_is_constructed_correctly()
+        {
+            var lines = File.ReadAllLines("test0.txt");
+            var field = new ConwayField(lines);
+
+            Assert.NotNull(field);
+            Assert.AreEqual('.', field[(-1, -1, 0)]);
+            Assert.AreEqual('#', field[(-1, 0, 0)]);
+            Assert.AreEqual('#', field[(0, 1, 0)]);
+            Assert.AreEqual('#', field[(1, -1, 0)]);
+            Assert.AreEqual('#', field[(1, 0, 0)]);
+            Assert.AreEqual('#', field[(1, 1, 0)]);
+            Assert.AreEqual(-1, field.MinX);
+            Assert.AreEqual(1, field.MaxX);
+            Assert.AreEqual(-1, field.MinY);
+            Assert.AreEqual(1, field.MaxY);
+            Assert.AreEqual(-1, field.MinZ);
+            Assert.AreEqual(1, field.MaxZ);
+        }
+
+        [Test]
+        public void It_parses_iterations_correctly()
+        {
+            var lines = File.ReadAllLines("test3.txt");
+            var field = new ConwayField(lines);
+
+            Assert.AreEqual(-3, field.MinX);
+            Assert.AreEqual(3, field.MaxX);
+            Assert.AreEqual(-3, field.MinY);
+            Assert.AreEqual(3, field.MaxY);
+            Assert.AreEqual(-3, field.MinZ);
+            Assert.AreEqual(3, field.MaxZ);
+        }
+
+        [Test]
+        public void It_counts_population_correctly()
+        {
+            var lines = File.ReadAllLines("test0.txt");
+            var field = new ConwayField(lines);
+
+            Assert.AreEqual(5, field.Population);
+        }
+
+        [Test]
+        public void It_counts_neighbors_correctly()
+        {
+            var lines = File.ReadAllLines("test3.txt");
+            var field = new ConwayField(lines);
+
+            Assert.AreEqual(7, field.NeighborCount(2, 2, 0));
+            Assert.AreEqual(0, field.NeighborCount(100, 100, 100));
+        }
+
+        [Test]
+        public void It_compares_fields_correctly()
+        {
+            var lines = File.ReadAllLines("test0.txt");
+            var f1 = new ConwayField(lines);
+            var f2 = new ConwayField(lines);
+            lines[0] = ".##";
+            var f3 = new ConwayField(lines);
+
+            Assert.IsTrue(f1.Equals(f1));
+            Assert.IsTrue(f1.Equals(f2));
+            Assert.IsFalse(f1.Equals(f3));
+        }
+
+        [Test]
+        public void It_iterates_correctly()
+        {
+            var field = new ConwayField(File.ReadAllLines("test0.txt"));
+            ConwayField test;
+            for (int i = 1; i <= 3; ++i)
+            {
+                test = new ConwayField(File.ReadAllLines($"test{i}.txt"));
+                field = field.Iterate();
+                Assert.IsTrue(test.Equals(field));
+            }
+        }
+    }
+}
