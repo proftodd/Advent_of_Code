eb8f828095309be07ccc51413a13e6d4ff3c76fc
John Todd <jtodd2854@gmail.com>
2023-12-05 17:50:08 -0500

    refactor the driver using dynamic import

--- AOC2023/index.ts
+++ AOC2023/index.ts
@@ -1,24 +1,26 @@
 import fs from 'fs'
 import readline from 'readline'
 import greeter from './src/greeter'
-import day_01 from './src/day_01/day'
-import day_02 from './src/day_02/day'
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-const getInput = (dayNumber: number): string[] => {
-    var filename = `../input/2023/Day_${dayNumber.toString().padStart(2, '0')}/input.txt`
+const getInput = (dayString: string): string[] => {
+    var filename = `../input/2023/${dayString}/input.txt`
     return fs.readFileSync(filename, 'utf8').split('\n')
         .filter(line => line != '')
 }
 
-let puzzleInput: string[]
 console.log(greeter(2023))
-rl.question('Which day would you like to run? ', s => {
-    puzzleInput = getInput(parseInt(s))
-    day_02(puzzleInput)
+rl.question('Which day would you like to run? ', async s => {
+    const dayString = `Day_${s.padStart(2, '0')}`
+    const puzzleInput = getInput(dayString)
+    // Reference for dynamic import:
+    // https://javascript.info/modules-dynamic-imports
+    const module = `./src/${dayString.toLowerCase()}/day`
+    const { default: process } = await import(module)
+    process(puzzleInput)
     rl.close()
 })
